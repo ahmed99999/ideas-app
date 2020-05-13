1380ddf2c19a447f5bb57cd722f683e42c102b63
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-05-13 19:47:03 +0200

    DEV/Ahmed: adding the return status + data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,25 @@
 import { Controller, Get, Body, Post } from '@nestjs/common';
-import { UsersService, User } from './users.service';
+import { UsersService, User, ResponseData } from './users.service';
 
 
 @Controller('users')
 export class UsersController {
-    constructor( private readonly userService: UsersService ){}
+    constructor(private readonly userService: UsersService) { }
 
     @Get()
-    getUsers(): User[]{
-        return this.userService.getUsers();
+    getUsers(): ResponseData<User[]> {
+        return {
+            status: "success",
+            data: this.userService.getUsers()
+        };
     }
 
     @Post()
-    addUser( @Body() body: User ){
-        const generatedUser = this.userService.createUser( body );
-        return generatedUser;
+    addUser(@Body() body: User): ResponseData<User> {
+        const generatedUser = this.userService.createUser(body);
+        return {
+            status: "success",
+            data: generatedUser
+        };
     }
 }
